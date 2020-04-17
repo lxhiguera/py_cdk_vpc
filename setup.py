1f82ad1a74b2cfaea5cee3afee077580a1efb43e
Luis Higuera <luis.higuera@escala24x7.com>
2020-04-17 03:16:58 -0500

    Update setup.py
    Update setup.py file to distribute packages

--- setup.py
+++ setup.py
@@ -9,17 +9,21 @@ setuptools.setup(
     name="py_cdk_vpc",
     version="0.0.1",
 
-    description="An empty CDK Python app",
+    description="Component for AWS CDK to Create VPC",
     long_description=long_description,
     long_description_content_type="text/markdown",
 
-    author="author",
+    author="Luis Higuera",
+    author_email="lxhiguera@gmail.com",
 
-    package_dir={"": "py_cdk_vpc"},
-    packages=setuptools.find_packages(where="py_cdk_vpc"),
+ #   package_dir={"": "py_cdk_vpc"},
+    packages=setuptools.find_packages(),
 
     install_requires=[
         "aws-cdk.core==1.32.2",
+        "aws-cdk.aws_iam==1.32.2",
+        "aws-cdk.aws_ec2==1.32.2",
+        "ipaddress==1.0.23"
     ],
 
     python_requires=">=3.6",
